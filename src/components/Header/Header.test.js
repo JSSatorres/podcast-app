4e0d620c053281ac0c212e5be09f00f3e40ac52a
jssatorres <juansataz.dev@gmail.com>
2023-07-16 13:52:07 +0200

    Feat: Header test

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,47 +1,53 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
 import { MemoryRouter } from 'react-router-dom'
+import { render, screen, waitFor } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
 import Header from './Header'
-import { LoadingContext } from '../../context/LoadingContext'
+import { useLoadingContextState } from '../../hook/useLoadingContextState'
 
-describe('Header', () => {
-  it('renders the header with the title', () => {
-    render(
-      <MemoryRouter>
-        <LoadingContext.Provider value={{ isLoading: false }}>
-          <Header />
-        </LoadingContext.Provider>
-      </MemoryRouter>
-    )
+jest.mock('../../hook/useLoadingContextState', () => ({
+  useLoadingContextState: jest.fn()
+}))
 
-    const titleLink = screen.getByText('Podcaster')
-    expect(titleLink).toBeInTheDocument()
-    expect(titleLink.getAttribute('href')).toBe('/')
+const renderElement = props => {
+  const utils = render(
+    <MemoryRouter>
+      <Header />
+    </MemoryRouter>
+  )
+  const query = {
+    titleLink: () => screen.getByText('Podcaster'),
+    loadingCircle: () => screen.queryByTestId('loading-circle')
+  }
+
+  return {
+    ...utils,
+    query
+  }
+}
+describe('Header Component', () => {
+  beforeEach(() => jest.clearAllMocks())
+  it('should render without crashes', async () => {
+    useLoadingContextState.mockReturnValue({ isLoading: false })
+    const { query } = renderElement()
+    expect(query.titleLink()).toBeInTheDocument()
   })
 
-  it('renders the loading circle when isLoading is true', () => {
-    render(
-      <MemoryRouter>
-        <LoadingContext.Provider value={{ isLoading: true }}>
-          <Header />
-        </LoadingContext.Provider>
-      </MemoryRouter>
-    )
+  it('should show loading circle when isLoading is true', async () => {
+    useLoadingContextState.mockReturnValue({ isLoading: true })
+    const { query } = renderElement()
 
-    const loadingCircle = screen.getByTestId('loading-circle')
-    expect(loadingCircle).toBeInTheDocument()
+    await waitFor(() => {
+      expect(query.loadingCircle()).toBeInTheDocument()
+    })
   })
 
-  it('does not render the loading circle when isLoading is false', () => {
-    render(
-      <MemoryRouter>
-        <LoadingContext.Provider value={{ isLoading: false }}>
-          <Header />
-        </LoadingContext.Provider>
-      </MemoryRouter>
-    )
+  it('should hide loading circle when isLoading is false', async () => {
+    useLoadingContextState.mockReturnValue({ isLoading: false })
+    const { query } = renderElement()
 
-    const loadingCircle = screen.queryByTestId('loading-circle')
-    expect(loadingCircle).toBeNull()
+    await waitFor(() => {
+      expect(query.loadingCircle()).toBeNull()
+    })
   })
 })
